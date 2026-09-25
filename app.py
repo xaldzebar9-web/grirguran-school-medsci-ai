449b094d11d02492b94fe063b490bb7a36cf1f9b
xaldzebar9-web <xaldzebar9@gmail.com>
2026-09-25 13:49:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def main():
     }
     
     model = genai.GenerativeModel(
-        model_name="gemini-2.5-flash",
+        model_name="gemini-3.6-flash",
         generation_config=generation_config,
         system_instruction=SYSTEM_PROMPT
     )
